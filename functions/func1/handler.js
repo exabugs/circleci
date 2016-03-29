f9219531f2d133145b1b7f75527d5635f88dd58c
exabugs <exabugs@gmail.com>
2016-03-29 19:08:49 +0900

    Hello

--- functions/func1/handler.js
+++ functions/func1/handler.js
@@ -2,6 +2,6 @@
 
 module.exports.handler = function(event, context) {
   return context.done(null, {
-    message: 'Go Serverless! Your Lambda function executed successfully!'
+    message: 'Hello!! Go Serverless! Your Lambda function executed successfully!'
   });
 };
