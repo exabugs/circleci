22672737147068590fdb614148da71f02ac300a4
exabugs <exabugs@gmail.com>
2016-03-29 20:16:00 +0900

    Fix serverless.sh.

--- scripts/serverless.sh
+++ scripts/serverless.sh
@@ -1,18 +1,7 @@
-
-#STAGE=${CIRCLE_BRANCH}
-STAGE=prod
-
-# slss project remove
-
-# sleep 10
-
-# slss project init -p ${AWS_PROFILE} -s ${STAGE} -r ${AWS_REGION}
-
-
+STAGE=dev
 
 OPTION="-s $STAGE -r $AWS_REGION"
 
-#sls project init -p $AWS_PROFILE $OPTION -c true
 sls project init -p $AWS_PROFILE $OPTION
 
 sls resources deploy $OPTION
@@ -20,3 +9,4 @@ sls resources deploy $OPTION
 sls function deploy $OPTION
 
 sls endpoint deploy $OPTION
+
